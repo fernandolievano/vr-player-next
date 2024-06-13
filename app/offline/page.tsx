8d35959bbe9ace1ab3409d0c152dbef3d82b4e2b
fernandolievano <fernandolievan@gmail.com>
2024-06-12 21:16:00 -0300

    chore: Update .gitignore and add PWA configuration
    - Update .gitignore to ignore auto-generated PWA files
    - Add PWA configuration to next.config.mjs

---
+++ app/offline/page.tsx
@@ -0,0 +1,7 @@
+export default function Offline() {
+  return (
+    <section className="px-0 py-0 min-h-screen w-full flex items-center justify-center bg-black">
+      <h1 className='font-bold text-white'>THIS IS AN OFFLINE SECTION</h1>
+    </section>
+  )
+}
